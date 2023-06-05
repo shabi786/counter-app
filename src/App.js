c76d762e28d68ab846b0aa811a18a6e6458d968e
shabiabbas <shabi.abbas@nagarro.com>
2023-06-05 14:30:01 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  let [count, setCount] = useState(0);
+  let [value, setValue] = useState(1);
+
+  const handleIncrement = () => {
+    setCount(count + value)
+  }
+
+  const handleDecrement = () => {
+    setCount(count - value)
+  }
+
+  const handleReset = () => {
+    setCount(0);
+  }
+
+  const handleAction = (inputNum) => {
+    if (inputNum) {
+      setValue(inputNum);
+    }
+    else {
+      setValue(1);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>Counter</h1>
+      <h2 style={{ color: count > 0 ? "green" : count < 0 ? "red" : "black" }}>{count}</h2>
+      <div>
+        <button className='increase' onClick={handleIncrement}>Increment</button>
+        <button className='decrease' onClick={handleDecrement}>Decrement</button>
+        <button className='reset' onClick={handleReset}>Reset</button>
+      </div>
+      <div className="input-container">
+        <label htmlFor='input'>Enter a number to perform action</label>
+        <input type="number"
+          name="input" id="input"
+          onChange={(e) => handleAction(e.target.valueAsNumber)} />
+      </div>
+
     </div>
   );
 }
